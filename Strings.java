1a6a9e208c5b53d65cc0e6c51f2266517293a3f0
shalinik21 <shalinikmr2001@gmail.com>
2023-08-13 18:58:48 +0530

    Initial commit

--- Strings.java
+++ Strings.java
@@ -3,5 +3,23 @@ public class Strings {
 
         String name = new String("Shalini");
         System.out.println(name);
+        //Strings objects are immutable ,they cannot be changed
+        //you can make its copy and change it
+
+        //Ways of printing 
+        System.out.print("Aloo ");
+        System.out.println("is potato.");
+        
+        String v ="Potato";
+        System.out.printf("He has 20 %s %n",v);
+
+        int x =12;
+        System.out.printf("%16d\n",x);
+
+        String date = "13th August";
+        System.out.printf("%16s\n",date);
+        
+        String u ="Mango";
+        System.out.format("He has 20 %s",u);
     }
 }
