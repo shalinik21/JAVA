a148ebeed885521546b9b5c2a759c5fefa90f620
shalinik21 <shalinikmr2001@gmail.com>
2023-08-14 01:06:09 +0530

    Initial commit

---
+++ EscapeSequence.java
@@ -0,0 +1,14 @@
+public class EscapeSequence {
+    public static void main(String args[]){
+
+        System.out.println("I am escape sequence \' double slash");
+        System.out.println("I am escape sequence \\ double slash");
+        System.out.println("I am escape sequence \" double slash");
+        System.out.println("I am escape sequence \t double slash");
+        System.out.println("I am escape sequence \r double slash");
+        System.out.println("I am escape sequence \f double slash");
+        System.out.println("I am escape sequence \b double slash");
+
+    }
+    
+}
