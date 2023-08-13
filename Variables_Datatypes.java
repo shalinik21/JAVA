97acfdac8c8db6e2883fc2c91cf23f90de8d2fb7
shalinik21 <shalinikmr2001@gmail.com>
2023-08-13 11:54:37 +0530

    Initial commit

---
+++ Variables_Datatypes.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+public class Variables_Datatypes {
+
+//Variables are like a container to store data
+//Types of data types : Primitive(int,byte ,float) & Non-primitive
+//The byte size of the boolean datatype depends on JDK
+
+     public static void main(String args[]){
+         int a =9;
+         int b =8;
+
+         System.out.println("The variable a contains the value :"+ a);
+         System.out.println("The variable b contains the value :"+ b);
+     }
+ }
+
+    
+
