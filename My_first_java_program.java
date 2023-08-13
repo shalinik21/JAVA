a570fd616f857137452ffe1daa37d8cc231edd78
shalinik21 <shalinikmr2001@gmail.com>
2023-08-13 10:04:53 +0530

    Add First program

---
+++ My_first_java_program.java
@@ -0,0 +1,30 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+//public class Main {
+//    public static void main(String[] args) {
+//        // Press Alt+Enter with your caret at the highlighted text to see how
+//        // IntelliJ IDEA suggests fixing it.
+//        System.out.printf("Hello and welcome!");
+//        System.out.println();
+//
+//        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+//        for (int i = 1; i <= 5; i++) {
+//
+//            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+//            // for you, but you can always add more by pressing Ctrl+F8.
+//            System.out.println("i = " + i);
+//        }
+//    }
+//}
+
+public class My_first_java_program{
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+    }
+}
+//NAMING Conventions
+
+//For classes we use PascalConvention
+//For functions we use camelCaseConvention
